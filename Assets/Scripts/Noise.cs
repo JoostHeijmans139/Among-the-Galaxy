5c423cef684ab4215b6f81a4ed64b30aed8af7ac
royboot <rgrizell467@gmail.com>
2025-09-16 23:25:33 +0200

    added GenerateNoiseMap function and moved noiseMap in there

--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public static class Noise{
-    public static List<List<float>> NoiseMap = new();
-    
+
+    public static List<List<float>> GenerateNoiseMap(int width, int height, float scale)
+    {
+        List<List<float>> noiseMap = new();
+        return noiseMap;
+    }
 }
 
