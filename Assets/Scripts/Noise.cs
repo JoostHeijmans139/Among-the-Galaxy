64da04795fac6dc323085ac03bbac1519cfc2dcd
royboot <rgrizell467@gmail.com>
2025-09-16 23:27:28 +0200

    added value checks for scale float should be between 0 and 1

--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -4,10 +4,18 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 public static class Noise{
-
+    //Scale value should only be between 0 and 1 not higher and not lower
     public static List<List<float>> GenerateNoiseMap(int width, int height, float scale)
     {
         List<List<float>> noiseMap = new();
+        if (scale <= 0)
+        {
+            scale = .0001f;
+        }
+        else if (scale >= 1)
+        {
+            Debug.LogError("Scale must be smaller then 1 it now is :" + scale);
+        }
         return noiseMap;
     }
 }
