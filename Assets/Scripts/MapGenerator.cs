de9ec3d3b629e98449d0d35272821eebd4a330dc
royboot <rgrizell467@gmail.com>
2025-09-17 00:12:20 +0200

    renamed class to MapGenerator

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RunTest : MonoBehaviour
+public class MapGenerator : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
