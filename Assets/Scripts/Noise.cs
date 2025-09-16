3b306a3c36a09258dfaebf0721d806f31c386a65
royboot <rgrizell467@gmail.com>
2025-09-16 23:42:00 +0200

    added logic to generate an actual List of Lists which could be used for a proper noise map

--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -10,11 +10,32 @@ public static class Noise{
         List<List<float>> noiseMap = new();
         if (scale <= 0)
         {
-            scale = .0001f;
+            Debug.LogError("Scale must be smaller then 0 it now is :" + scale);
         }
         else if (scale >= 1)
         {
             Debug.LogError("Scale must be smaller then 1 it now is :" + scale);
+        }
+
+        for (int y = 0; y < height; y++)
+        {
+            List<float> row = new();
+            for (int x = 0; x < width; x++)
+            {
+                float sampleX = x / scale;
+                float sampleY = y / scale;
+                float noise = Mathf.PerlinNoise(sampleX, sampleY);
+                row.Add(noise);
+                Debug.Log(noise);
+            }
+            noiseMap.Add(row);
+        }
+        foreach (var row in noiseMap)
+        {
+            foreach (var noise in row)
+            {
+                Debug.Log(noise);
+            }   
         }
         return noiseMap;
     }
