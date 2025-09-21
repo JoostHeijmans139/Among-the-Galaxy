628f4b06b049657bd993794b4810602d4d3cc2aa
royboot <rgrizell467@gmail.com>
2025-09-21 21:59:38 +0200

    added a display option for displaying the mesh

--- Assets/Scripts/DisplayMap.cs
+++ Assets/Scripts/DisplayMap.cs
@@ -4,7 +4,8 @@ public class DisplayMap : MonoBehaviour
 {
     //Define a renderer to display the texture
     public Renderer textureRenderer;
-    
+    public MeshFilter meshFilter;
+    public MeshRenderer meshRenderer;
     /// <summary>
     /// This function generates and applies a 2d texture based on the provided noise map to visualize the noise values.
     /// </summary>
@@ -23,6 +24,13 @@ public class DisplayMap : MonoBehaviour
         SetFilterOptions(texture);
         textureRenderer.sharedMaterial.mainTexture = texture;
         textureRenderer.transform.localScale = new Vector3(texture.width,1,texture.height);
+    }
+
+    public void DrawMesh(MeshData meshData,Texture2D texture)
+    {
+        meshFilter.sharedMesh = meshData.CreateMesh();
+        meshRenderer.sharedMaterial.mainTexture = texture;
+        meshRenderer.transform.localScale = new Vector3(texture.width,1,texture.height);
     }
 
     private void SetFilterOptions(Texture2D texture)
