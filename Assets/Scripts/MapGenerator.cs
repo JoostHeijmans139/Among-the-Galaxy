666290b77cf3d312c3cbb4afb65294f545973ee8
Royboot <rgrizell467@gmail.com>
2025-09-23 14:35:14 +0200

    added range to octaves

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -14,6 +14,7 @@ public class MapGenerator:MonoBehaviour
     public int mapHeight;
     [Range(2,100)]
     public float noiseScale;
+    [Range(0,20)]
     public int octaves;
     [Range(0, 1)]
     public float persistence;
