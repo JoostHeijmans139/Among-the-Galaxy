6c81d15308a60c1c0fdf8398e7f28d8572224b40
royboot <rgrizell467@gmail.com>
2025-10-11 11:29:31 +0200

    added function to Load a default set of settings

--- Assets/Scripts/Settings/SettingsManager.cs
+++ Assets/Scripts/Settings/SettingsManager.cs
@@ -12,13 +12,14 @@ namespace Settings
         {
             LoadSettings();
         }
-        
+
         public static void SaveSettings()
         {
             string json = JsonUtility.ToJson(CurrentSettings, true);
             System.IO.File.WriteAllText(SettingsPath, json);
             Debug.Log($"Settings saved to {SettingsPath}");
         }
+
         private static void LoadSettings()
         {
             if (System.IO.File.Exists(SettingsPath))
@@ -29,11 +30,21 @@ namespace Settings
             }
             else
             {
-                Debug.LogWarning($"Settings file not found at {SettingsPath}. Using default settings.");
-                CurrentSettings = new SettingsData();
+                Debug.LogWarning($"Settings file not found at {SettingsPath}. Creating default settings.");
+                LoadDefaultSettings();
             }
         }
-    }
+
+        private static void LoadDefaultSettings()
+        {
+            CurrentSettings = new SettingsData
+            {
+                levelOfDetail = 0
+            };
+            SaveSettings();
+            LoadSettings();
+        }
+}
     [Serializable]
     public class SettingsData
     {
