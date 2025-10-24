40c3f768c8a6452c2892130b5638d23d984b4807
WesselBiemans <119673590+WesselBiemans@users.noreply.github.com>
2025-10-24 16:41:36 +0200

    SelectionManager works*
    *It currently only works if the player is walking backwards while looking at the rock. It also only works with the rock.
    
    Will fix in future

---
+++ Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+/// <summary>
+/// This class manages the selection of interactable objects in the scene.
+/// It uses raycasting to detect objects under the mouse cursor and displays.
+/// Then it shows interaction information if the object is interactable.
+public class SelectionManager : MonoBehaviour
+{
+    public GameObject InteractionInfo;
+    private TMP_Text interactionInfoText;
+
+    public Camera playerCamera;
+    public float maxDistance = 5f;
+
+    //interaction info text set to the text mesh pro component
+    private void Start()
+    {
+        interactionInfoText = InteractionInfo.GetComponent<TMP_Text>();
+        InteractionInfo.SetActive(false);
+    }
+
+    void Update()
+    {
+        Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, maxDistance))
+        {
+            // Debug line to visualize the raycast in the scene view
+            Debug.DrawLine(ray.origin, hit.point, Color.red);
+
+            var selectionTransform = hit.transform;
+            InteractableObject interactable = selectionTransform.GetComponent<InteractableObject>();
+
+            if (interactable != null)
+            {
+                interactionInfoText.text = interactable.GetItemName();
+                InteractionInfo.SetActive(true);
+            }
+            else
+            {
+                InteractionInfo.SetActive(false);
+            }
+        }
+        else
+        {
+            Debug.DrawLine(ray.origin, ray.origin + ray.direction * maxDistance, Color.green);
+        }
+    }
+
+}
